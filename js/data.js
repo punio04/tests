a902af769c69c6edcfdaa27c6939e3ba5cd53d87
あかり <85103259+punio04@users.noreply.github.com>
2022-05-15 16:57:58 +0900

    Add files via upload

---
+++ js/data.js
@@ -0,0 +1,280 @@
+export const dataApps = [
+  {
+    label: "PAIRS",
+    name: "Pairs",
+    url: "https://ad.presco.asia/cl/?b_id=bEpScxeH&t_id=1&afad_param_1=【top】",
+    imgPath: "./images/app_icon1.png",
+    trSource: "<td><a href='https://ad.presco.asia/cl/?b_id=bEpScxeH&t_id=1&afad_param_1=【top】'><img src=./images/app_icon1.png><span class=app>Pairs</span></a><td><span class=number>1500万人</span><td><span class='tag bg1'>恋活</span><span class='tag bg2'>婚活</span><td><span class=age>20代</span><span class=age>30代</span><td><a href=https://matchinglike.jp/ >マッチングアプリ</a></td>"
+  },
+  {
+    label: "HAPPY_MAIL",
+    name: "ハッピーメール",
+    url: "https://ad.presco.asia/cl/?b_id=t14WioXv&t_id=1&afad_param_1=【top】",
+    imgPath: "./images/app_icon4.png",
+    trSource: "<td><a href='https://ad.presco.asia/cl/?b_id=t14WioXv&t_id=1&afad_param_1=【top】'><img src=./images/app_icon4.png><span class=app>ハッピーメール</span></a><td><span class=number>2500万人</span><td><span class='tag bg3'>出会い</span><span class='tag bg4'>友達作り</span><td><span class=age>10代</span><span class=age>20代</span><span class=age>30代</span><td><a href=https://matchinglike.jp/ >出会い系</a></td>"
+
+  },
+  {
+    label: "WITH",
+    name: "With",
+    url: "https://ad.presco.asia/cl/?b_id=LN2gGUeC&t_id=1&afad_param_1=【top】",
+    imgPath: "./images/app_icon2.png",
+    trSource: "<td><a href='https://ad.presco.asia/cl/?b_id=LN2gGUeC&t_id=1&afad_param_1=【top】'><img src=./images/app_icon2.png><span class=app>With</span></a><td><span class=number>500万人</span><td><span class='tag bg1'>恋活</span><span class='tag bg3'>出会い</span><td><span class=age>20代</span><span class=age>30代</span><td><a href=https://matchinglike.jp/ >マッチング アプリ</a></td>"
+
+  },
+  {
+    label: "WAKUWAKU_MAIL",
+    name: "ワクワクメール",
+    url: "https://ad.presco.asia/cl/?b_id=YcJlp6WB&t_id=1&afad_param_1=【top】",
+    imgPath: "images/app_icon5.png",
+    trSource: "<td><a href='https://ad.presco.asia/cl/?b_id=YcJlp6WB&t_id=1&afad_param_1=【top】'><img src=./images/app_icon5.png><span class=app>ワクワクメール</span></a><td><span class=number>1600万人</span><td><span class='tag bg3'>出会い</span><span class='tag bg4'>友達作り</span><td><span class=age>10代</span><span class=age>20代</span><td><a href=https://matchinglike.jp/ >マッチングアプリ</a></td>"
+
+  },
+  {
+    label: "PCMAX",
+    name: "PCMAX",
+    url: "https://ad.presco.asia/cl/?b_id=k6uQ5OLD&t_id=1&afad_param_1=【top】",
+    imgPath: "images/app_icon6.png",
+    trSource: "<td><a href='https://ad.presco.asia/cl/?b_id=k6uQ5OLD&t_id=1&afad_param_1=【top】'><img src=./images/app_icon6.png><span class=app>PCMAX</span></a><td><span class=number>1600万人</span><td><span class='tag bg3'>出会い</span><span class='tag bg4'>友達作り</span><td><span class=age>20代</span><span class=age>30代</span><span class=age>40代</span><td><a href=https://matchinglike.jp/ >出会い系</a></td>"
+
+  },
+  {
+    label: "TAPPLE",
+    name: "タップル",
+    url: "https://tapple.me/",
+    imgPath: "images/app_icon3.png",
+    trSource: "<td><a href=https://tapple.me/ ><img src=./images/app_icon3.png><span class=app>タップル</span></a><td><span class=number>600万人</span><td><span class='tag bg1'>恋活</span><span class='tag bg2'>婚活</span><td><span class=age>10代</span><span class=age>20代</span><td><a href=https://matchinglike.jp/ >マッチングアプリ</a></td>"
+
+  },
+  {
+    label: "MATCH",
+    name: "Match",
+    url: "https://ad.presco.asia/cl/?b_id=mNYohyiT&t_id=1&afad_param_1=【top】",
+    imgPath: "images/app_icon8.png",
+    trSource: "<td><a href='https://ad.presco.asia/cl/?b_id=mNYohyiT&t_id=1&afad_param_1=【top】'><img src=./images/app_icon8.png><span class=app>Match</span></a><td><span class=number>非公開</span><td><span class='bg2 tag'>婚活</span><td><span class=age>30代</span><span class=age>40代</span><td><a href=https://matchinglike.jp/ >婚活<br>アプリ</a></td>"
+
+  },
+  {
+    label: "MARRISH",
+    name: "マリッシュ",
+    url: "https://ad.presco.asia/cl/?b_id=Pse58i9g&t_id=1&afad_param_1=【top】",
+    imgPath: "images/app_icon7.png",
+    trSource: "<td><a href='https://ad.presco.asia/cl/?b_id=Pse58i9g&t_id=1&afad_param_1=【top】'><img src=./images/app_icon7.png><span class=app>マリッシュ</span></a><td><span class=number>100万人</span><td><span class='bg2 tag'>婚活</span><td><span class=age>30代</span><span class=age>40代</span><td><a href=https://matchinglike.jp/ >婚活<br>アプリ</a></td>"
+
+  },
+  {
+    label: "AOCCA",
+    name: "aocca",
+    url: "https://ad.presco.asia/cl/?b_id=EX54QP2u&t_id=1&afad_param_1=【top】",
+    imgPath: "images/app_icon11.png",
+    trSource: "<tr><td><a href='https://ad.presco.asia/cl/?b_id=EX54QP2u&amp;t_id=1'><img src='./images/app_icon11.png'><span class='app'>aocca</span></a></td><td><span class='number'>44.7万人</span></td><td><span class='tag bg1'>恋活</span><span class='tag bg3'>婚活</span></td><td><span class='age'>30代</span><span class='age'>40代</span></td><td><a href='https://matchinglike.jp/'>マッチング アプリ</a></td></tr>"
+
+  },
+  {
+    label: "ZEXY_ENMUSUBI",
+    name: "ゼクシィ縁結び",
+    url: "https://zexy-enmusubi.net/",
+    imgPath: "images/app_icon9.png",
+    trSource: "<td><a href=https://zexy-enmusubi.net/ ><img src=./images/app_icon9.png><span class=app>ゼクシィ縁結び</span></a><td><span class=number>140万人</span><td><span class='bg2 tag'>婚活</span><td><span class=age>30代</span><span class=age>40代</span><td><a href=https://matchinglike.jp/ >婚活<br>アプリ</a></td>"
+  }
+]
+
+export const dataQuestions = [
+  {
+    no: 1,
+    question: "あなたの性別は？",
+    answers: [
+      {
+        answer: "男性",
+        results: ["PAIRS", "HAPPY_MAIL", "WITH", "WAKUWAKU_MAIL", "PCMAX", "TAPPLE", "MATCH", "MARRISH", "AOCCA", "ZEXY_ENMUSUBI"]
+      },
+      {
+        answer: "女性",
+        results: ["PAIRS", "HAPPY_MAIL", "WITH", "WAKUWAKU_MAIL", "PCMAX", "TAPPLE", "MATCH", "MARRISH", "AOCCA", "ZEXY_ENMUSUBI"]
+      }
+    ]
+  },
+  {
+    no: 2,
+    question: "あなたの年齢層は？",
+    answers: [
+      {
+        answer: "24歳以下",
+        results: ["PAIRS", "HAPPY_MAIL", "WITH", "WAKUWAKU_MAIL", "PCMAX", "TAPPLE"]
+      },
+      {
+        answer: "20代後半",
+        results: ["PAIRS", "HAPPY_MAIL", "WITH", "WAKUWAKU_MAIL", "PCMAX", "TAPPLE"]
+      },
+      {
+        answer: "30代前半",
+        results: ["PAIRS", "HAPPY_MAIL", "WITH", "PCMAX", "MATCH", "MARRISH", "AOCCA", "ZEXY_ENMUSUBI"]
+      },
+      {
+        answer: "30代後半",
+        results: ["PAIRS", "HAPPY_MAIL", "WITH", "PCMAX", "MATCH", "MARRISH", "AOCCA", "ZEXY_ENMUSUBI"]
+      },
+      {
+        answer: "40代以上",
+        results: ["HAPPY_MAIL", "PCMAX", "MATCH", "MARRISH", "AOCCA", "ZEXY_ENMUSUBI"]
+      }
+    ]
+  },
+  {
+    no: 3,
+    question: "マッチングアプリを使う目的は？",
+    answers: [
+      {
+        answer: "恋活",
+        results: ["PAIRS", "WITH", "TAPPLE", "AOCCA"]
+      },
+      {
+        answer: "婚活",
+        results: ["PAIRS", "TAPPLE", "MATCH", "MARRISH", "AOCCA", "ZEXY_ENMUSUBI"]
+      },
+      {
+        answer: "デート",
+        results: ["HAPPY_MAIL", "WITH", "WAKUWAKU_MAIL", "PCMAX"]
+      }
+    ]
+  },
+  {
+    no: 4,
+    question: "いつまでに会いたい？",
+    answers: [
+      {
+        answer: "今すぐ",
+        results: ["PAIRS", "HAPPY_MAIL", "WAKUWAKU_MAIL", "PCMAX", "TAPPLE", "AOCCA"]
+      },
+      {
+        answer: "~3ヶ月以内",
+        results: ["PAIRS", "HAPPY_MAIL", "WITH", "WAKUWAKU_MAIL", "PCMAX", "TAPPLE", "AOCCA"]
+      },
+      {
+        answer: "~半年以内",
+        results: ["PAIRS", "WITH", "MATCH", "MARRISH", "AOCCA", "ZEXY_ENMUSUBI"]
+      },
+      {
+        answer: "特にない",
+        results: ["PAIRS", "HAPPY_MAIL", "WITH", "WAKUWAKU_MAIL", "PCMAX", "TAPPLE", "MATCH", "MARRISH", "AOCCA", "ZEXY_ENMUSUBI"]
+      }
+    ]
+  }
+]
+
+export const dataSearch = [
+  {
+    no: 1,
+    label: "purpose",
+    select: [
+      {
+        value: "利用目的",
+        results: ["PAIRS", "HAPPY_MAIL", "WITH", "WAKUWAKU_MAIL", "PCMAX", "TAPPLE", "MATCH", "MARRISH", "AOCCA", "ZEXY_ENMUSUBI"]
+      },
+      {
+        value: "恋活",
+        results: ["PAIRS", "WITH", "TAPPLE", "AOCCA"]
+      },
+      {
+        value: "婚活",
+        results: ["PAIRS", "TAPPLE", "MATCH", "MARRISH", "AOCCA", "ZEXY_ENMUSUBI"]
+      },
+      {
+        value: "出会い",
+        results: ["HAPPY_MAIL", "WITH", "WAKUWAKU_MAIL", "PCMAX"]
+      },
+      {
+        value: "友達作り",
+        results: ["HAPPY_MAIL", "WAKUWAKU_MAIL", "PCMAX"]
+      }
+    ]
+  },
+  {
+    no: 2,
+    label: "age",
+    select: [
+      {
+        value: "年齢層",
+        results: ["PAIRS", "HAPPY_MAIL", "WITH", "WAKUWAKU_MAIL", "PCMAX", "TAPPLE", "MATCH", "MARRISH", "AOCCA", "ZEXY_ENMUSUBI"]
+      },
+      {
+        value: "10代",
+        results: ["HAPPY_MAIL", "WAKUWAKU_MAIL", "TAPPLE"]
+      },
+      {
+        value: "20代",
+        results: ["PAIRS", "HAPPY_MAIL", "WITH", "WAKUWAKU_MAIL", "PCMAX", "TAPPLE"]
+      },
+      {
+        value: "30代",
+        results: ["PAIRS", "HAPPY_MAIL", "WITH", "PCMAX", "MATCH", "MARRISH", "AOCCA", "ZEXY_ENMUSUBI"]
+      },
+      {
+        value: "40代",
+        results: ["HAPPY_MAIL", "PCMAX", "MATCH", "AOCCA", "ZEXY_ENMUSUBI"]
+      }
+    ]
+  },
+  {
+    no: 3,
+    label: "price",
+    select: [
+      {
+        value: "料金",
+        results: ["PAIRS", "HAPPY_MAIL", "WITH", "WAKUWAKU_MAIL", "PCMAX", "TAPPLE", "MATCH", "MARRISH", "AOCCA", "ZEXY_ENMUSUBI"]
+      },
+      {
+        value: "定額制",
+        results: ["PAIRS", "WITH", "TAPPLE", "MATCH", "MARRISH", "AOCCA", "ZEXY_ENMUSUBI"]
+      },
+      {
+        value: "ポイント制",
+        results: ["HAPPY_MAIL", "WAKUWAKU_MAIL", "PCMAX"]
+      }
+    ]
+  },
+  {
+    no: 4,
+    label: "membership",
+    select: [
+      {
+        value: "会員数",
+        results: ["PAIRS", "HAPPY_MAIL", "WITH", "WAKUWAKU_MAIL", "PCMAX", "TAPPLE", "MATCH", "MARRISH", "AOCCA", "ZEXY_ENMUSUBI"]
+      },
+      {
+        value: "100万人以上",
+        results: ["PAIRS", "HAPPY_MAIL", "WITH", "WAKUWAKU_MAIL", "PCMAX", "TAPPLE", "MARRISH", "OMIAI", "ZEXY_ENMUSUBI"]
+      },
+      {
+        value: "300万人以上",
+        results: ["PAIRS", "HAPPY_MAIL", "WITH", "WAKUWAKU_MAIL", "PCMAX", "TAPPLE", "OMIAI"]
+      },
+      {
+        value: "500万人以上",
+        results: ["PAIRS", "HAPPY_MAIL", "WITH", "WAKUWAKU_MAIL", "PCMAX", "TAPPLE", "OMIAI"]
+      },
+      {
+        value: "1000万人以上",
+        results: ["PAIRS", "HAPPY_MAIL", "WAKUWAKU_MAIL", "PCMAX"]
+      }
+    ]
+  },
+  {
+    no: 5,
+    label: "release",
+    select: [
+      {
+        value: "リリース日",
+        results: ["PAIRS", "HAPPY_MAIL", "WITH", "WAKUWAKU_MAIL", "PCMAX", "TAPPLE", "MATCH", "MARRISH", "AOCCA", "ZEXY_ENMUSUBI"]
+      },
+      {
+        value: "2015年以前",
+        results: ["PAIRS", "HAPPY_MAIL", "WAKUWAKU_MAIL", "PCMAX", "TAPPLE", "MATCH"]
+      },
+      {
+        value: "2015年以降",
+        results: ["WITH", "MARRISH", "AOCCA", "ZEXY_ENMUSUBI"]
+      }
+    ]
+  }
+]
